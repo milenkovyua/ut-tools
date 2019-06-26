ee145b8983ba829a1306b0aed7cd5589d11cedf7
stamen.peev <stamen.peev@softwaregroup-bg.com>
2019-06-26 16:33:24 +0300

    fix: versionBump can now be used for checking version correctness and generating next version and tag without actually bumping

--- bin/ut-versionbump.js
+++ bin/ut-versionbump.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 /* eslint no-console:0, no-process-exit:0 */
-const versionBump = require('../lib/versionBump');
-versionBump()
+
+const bump = ['0', 'false', 'skip'].indexOf(process.argv[2]) === -1;
+
+require('../lib/versionBump')(bump)
     .catch((e) => {
         console.error(e);
         process.exit(1);
